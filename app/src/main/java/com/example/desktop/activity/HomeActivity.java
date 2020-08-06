27072b8bf4602ee271a169c1d3d0ccb54906fa45
waitwalker <waitwalker@163.com>
2020-08-06 17:23:35 +0800

    TabBar 相关配置添加

--- app/src/main/java/com/example/desktop/activity/HomeActivity.java
+++ app/src/main/java/com/example/desktop/activity/HomeActivity.java
@@ -3,13 +3,48 @@ package com.example.desktop.activity;
 
 import android.os.Bundle;
 
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
 import com.example.desktop.R;
+import com.example.desktop.entity.TabEntity;
+import com.example.desktop.fragment.CollectFragment;
+import com.example.desktop.fragment.HomeFragment;
+import com.example.desktop.fragment.MyFragment;
+import com.flyco.tablayout.CommonTabLayout;
+import com.flyco.tablayout.listener.CustomTabEntity;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends BaseActivity {
+
+    private ArrayList<Fragment> mFragments = new ArrayList<>();
+    private ArrayList<CustomTabEntity> mTabEntities = new ArrayList<>();
+    private String[] mTitles = {"首页", "收藏", "我的"};
+    private int[] mIconUnSelectIds = {
+            R.mipmap.home_unselect, R.mipmap.collect_unselect,
+            R.mipmap.my_unselect};
+    private int[] mIconSelectIds = {
+            R.mipmap.home_selected, R.mipmap.collect_selected,
+            R.mipmap.my_selected};
+
+    private ViewPager viewPager;
+    private CommonTabLayout commonTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        viewPager = findViewById(R.id.viewpager);
+        commonTabLayout = findViewById(R.id.commonTabLayout);
+
+        mFragments.add(HomeFragment.newInstance("1","2"));
+        mFragments.add(CollectFragment.newInstance("1","2"));
+        mFragments.add(MyFragment.newInstance("1","2"));
+
+        for (int i = 0; i < mTitles.length; i++) {
+            mTabEntities.add(new TabEntity(mTitles[i], mIconSelectIds[i], mIconUnSelectIds[i]));
+        }
+
     }
 }

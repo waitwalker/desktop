4320116b179e4484dd8293d8cb301a32e51f872e
waitwalker <waitwalker@163.com>
2020-08-06 09:39:12 +0800

    登录跳转添加

--- app/src/main/java/com/example/desktop/MainActivity.java
+++ app/src/main/java/com/example/desktop/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.desktop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.desktop.activity.LoginActivity;
+
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnClickListener {
+
+    Button loginButton;
+    Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = findViewById(R.id.login_button);
+        registerButton = findViewById(R.id.register_button);
+
+        loginButton.setOnClickListener(this);
+        registerButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login_button:
+                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+                break;
+            case R.id.register_button:
+                break;
+        }
     }
 }
